19fd12a0916e3d883f3ace4a84be5d1d5b4f3e82
Root99 <azizrezgui4@gmail.com>
2023-07-01 01:58:45 +0100

    added client

---
+++ client/src/App.tsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+function App() {
+  const GET_GAMES = gql`
+    query GetGames {
+      games {
+        title
+        reviews {
+          rating
+          author {
+            name
+          }
+        }
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(GET_GAMES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  return (
+    <div>
+      {data.games.map((game) => (
+        <div>
+          <h1>{game.title}</h1>
+          <span>{game.reviews.map((r) => r.rating)}</span>
+          <span>{game.reviews.map((r) => r.author.name)}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
